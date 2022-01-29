23831f44ed003f6c95d028832d10f50269cf3f98
Luis <tonyyluis@hotmail.com>
2022-01-28 20:59:43 -0500

    player prefab, monster state added

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody
 
 export var speed = 15
-#export var acceleration = 20
+export var mSpeed = 5
+export var isMonster = false;
 var velocity = Vector3.ZERO
 export var speedLock = 0
 export var boost = 10
@@ -14,37 +15,40 @@ func _physics_process(delta):
 		direction.x -= 1
 	if Input.is_action_pressed("left"):
 		direction.x +=1
-	if Input.is_action_pressed(("foreward")):
+	if Input.is_action_pressed(("forward")):
 		direction.z += 1
 	if Input.is_action_pressed("back"):
 		direction.z -= 1
 		
-	#if abs(velocity.x) <= 0 && abs(velocity.z) <= 0:
-	#	speedLock = true
+
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(translation + direction, Vector3.UP)
-	#if (abs(velocity.x) < speed && abs(velocity.z) < speed) && speedLock:
-	#	velocity.x += acceleration*delta*direction.x
-	#	velocity.z += acceleration*delta*direction.z
+
+	if !isMonster:
 		
-		print(velocity.x)
-	
-	if Input.is_action_just_pressed(" Boost") && direction != Vector3.ZERO:
-		velocity.x = direction.x*speed*boost
-		velocity.z = direction.z*speed*boost
+		if Input.is_action_just_pressed("boost") && direction != Vector3.ZERO:
+			velocity.x = direction.x*speed*boost
+			velocity.z = direction.z*speed*boost
+		else:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
 	else:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-	# Vertical velocity
-	#velocity.y -= fall_acceleration * delta ##unused##
-	# Moving the character
+		if Input.is_action_just_pressed("boost") && direction != Vector3.ZERO:
+			velocity.x = direction.x*mSpeed*boost
+			velocity.z = direction.z*mSpeed*boost
+		else:
+			velocity.x = direction.x * mSpeed
+			velocity.z = direction.z * mSpeed
+			
 	velocity = move_and_slide(velocity, Vector3.UP)
-
-
+	
+	if Input.is_action_just_pressed("monster"):
+		isMonster = !isMonster
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	 # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
