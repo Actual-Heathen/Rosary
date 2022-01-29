c56fab94ee7760d8956e56f965ec94e9d54be1db
Kalpan <54441494+kalpan345@users.noreply.github.com>
2022-01-29 17:39:36 -0500

    temp

--- scenes/enemyBaseModel.gd
+++ scenes/enemyBaseModel.gd
@@ -1,22 +1,35 @@
 extends KinematicBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
+export var botSpeed = 5
+export var botOffset = Vector3(5,0,5)
+var velocity = Vector3(botSpeed,0,5)
+const ACCEL = 5.0
+const DEACCEL = 20.0
+const MAX_SPEED = 2.0
+const ROT_SPEED = 1.0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var prev_advance = false
+var dying = false
+var rot_dir = 4
 
-var velocity = Vector3(100,0,0)
+onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 
-#func _physics_process(delta): 
-	#move_and_slide(velocity, Vector3(0,-1,-1))
+
+func _physics_process(delta): 
+
+
+	print(translation)
+
+	if (translation.x <= 5):
+		print('----------------------------------------------------------------------------------')
+		move_and_slide(velocity, botOffset)
+	elif (translation.x > 5) :
+		print('************************************************************************************')
+		move_and_slide(-velocity, -botOffset)
+
+	#print(get_floor_normal())
+	#print('------')
 	
+func _die():
+	queue_free()
