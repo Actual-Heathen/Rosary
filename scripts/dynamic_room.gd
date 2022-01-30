5ec5e2a8cbdcfdb44639873fbff71a222195603f
Hayden Estes <hayden@Hayden-T440p>
2022-01-30 00:57:26 -0600

    dyanmic rooms walls are even more dynamic

--- scripts/dynamic_room.gd
+++ scripts/dynamic_room.gd
@@ -4,23 +4,28 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var wall1_mat = $wall1/MeshInstance
-onready var wall2_mat = $wall2/MeshInstance
-onready var wall3_mat = $wall3/MeshInstance
-onready var wall4_mat = $wall4/MeshInstance
+onready var wall_left_mat = $wall1/MeshInstance
+onready var wall_right_mat = $wall2/MeshInstance
+onready var wall_top_mat = $wall3/MeshInstance
+onready var wall_bottom_mat = $wall4/MeshInstance
 onready var ground_mat = $ground/MeshInstance
 
-onready var wall1_col = $wall1/CollisionShape
-onready var wall2_col = $wall2/CollisionShape
-onready var wall3_col = $wall3/CollisionShape
-onready var wall4_col = $wall4/CollisionShape
+onready var wall_left_col = $wall1/CollisionShape
+onready var wall_right_col = $wall2/CollisionShape
+onready var wall_top_col = $wall3/CollisionShape
+onready var wall_bottom_col = $wall4/CollisionShape
 onready var ground_col = $ground/CollisionShape
 
-onready var wall1 = $wall1
-onready var wall2 = $wall2
-onready var wall3 = $wall3
-onready var wall4 = $wall4
+onready var wall_left = $wall1
+onready var wall_right = $wall2
+onready var wall_top = $wall3
+onready var wall_bottom = $wall4
 onready var ground = $ground
+
+export (bool) var left_wall
+export (bool) var right_wall
+export (bool) var top_wall
+export (bool) var bottom_wall
 
 const centerMaterials : Array = [
 	preload("res://materials/stone.tres"),
@@ -39,21 +44,21 @@ func _ready():
 	if floor_name == "floor3":
 		floor_num = 2
 
-	wall1_mat.set_surface_material(0, centerMaterials[floor_num])
-	wall2_mat.set_surface_material(0, centerMaterials[floor_num])
-	wall3_mat.set_surface_material(0, centerMaterials[floor_num])
-	wall4_mat.set_surface_material(0, centerMaterials[floor_num])
+	wall_left_mat.set_surface_material(0, centerMaterials[floor_num])
+	wall_right_mat.set_surface_material(0, centerMaterials[floor_num])
+	wall_top_mat.set_surface_material(0, centerMaterials[floor_num])
+	wall_bottom_mat.set_surface_material(0, centerMaterials[floor_num])
 	ground_mat.set_surface_material(0, centerMaterials[floor_num])
 
-	wall1_mat.visible = false
-	wall2_mat.visible = false
-	wall3_mat.visible = false
-	wall4_mat.visible = false
+	wall_left_mat.visible = left_wall
+	wall_right_mat.visible = right_wall
+	wall_top_mat.visible = top_wall
+	wall_bottom_mat.visible = bottom_wall
 
-	wall1_col.disabled = true
-	wall2_col.disabled = true
-	wall3_col.disabled = true
-	wall4_col.disabled = true
+	wall_left_col.disabled = left_wall
+	wall_right_col.disabled = right_wall
+	wall_top_col.disabled = top_wall
+	wall_bottom_col.disabled = bottom_wall
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
