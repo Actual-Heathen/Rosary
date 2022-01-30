c84340725d1b0a046501b73b0deda7a12349c437
Kalpan <54441494+kalpan345@users.noreply.github.com>
2022-01-30 10:56:57 -0500

    Update enemyBaseModel.gd

--- scripts/enemyBaseModel.gd
+++ scripts/enemyBaseModel.gd
@@ -3,17 +3,17 @@ extends KinematicBody
 export var botSpeed = 15
 export var botOffset = Vector3(5,0,5)
 export var startingPosition = Vector3.ZERO
-
+export var chaseRadius = 10
 var velocity = Vector3(botSpeed,0,5)
 onready var player = get_node("../../player")
 
 func _physics_process(delta):
-	if (translation.distance_to(player.global_transform.origin)>5 && translation.distance_to(startingPosition)<5):
+	if (translation.distance_to(player.global_transform.origin)>chaseRadius && translation.distance_to(startingPosition)<chaseRadius):
 		var patrolDir = (getRoamingPosition() - global_transform.origin).normalized() 
 		patrolDir.y = 0
 		move_and_slide(patrolDir * botSpeed)
 	
-	elif (translation.distance_to(player.global_transform.origin)<5):
+	elif (translation.distance_to(player.global_transform.origin)<chaseRadius):
 		var direction = (player.global_transform.origin - global_transform.origin).normalized()
 		move_and_slide(direction * botSpeed)
 		
@@ -29,7 +29,7 @@ func getRandomDir():
 func getRoamingPosition():
 	var roamDir = startingPosition + getRandomDir()*rand_range(20,30)
 	return roamDir
-	
+
 func _ready():
 	startingPosition = global_transform.origin
 	pass # Replace with function body.
