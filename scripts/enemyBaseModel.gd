b1d9fac31fe3bbfbb5b2073084428ba1c3bdee34
Kalpan <54441494+kalpan345@users.noreply.github.com>
2022-01-29 20:59:34 -0500

    Enemy Change Behaviors

--- scripts/enemyBaseModel.gd
+++ scripts/enemyBaseModel.gd
@@ -4,23 +4,12 @@ export var botSpeed = 15
 export var botOffset = Vector3(5,0,5)
 var velocity = Vector3(botSpeed,0,5)
 
-const GRAVITY = 600
-
-#var health = 5
 
 onready var player = get_node("../../player")
-onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * ProjectSettings.get_setting("physics/3d/default_gravity_vector")
-
-var initialPos = translation
-#func _physics_process(delta): 
-#	print(self.get_position_in_parent())
-#	if ((translation.x <= initialPos.x + botOffset.x) ||  (translation.z <= initialPos.z + botOffset.z)):
-#		print('----------------------------------------------------------------------------------')
-#		move_and_slide(velocity, Vector3(-5,0,5))
 
 func _physics_process(delta):
-	if player:
-		var direction = (player.global_transform.origin - self.global_transform.origin).normalized()
+	if (translation.distance_to(player.global_transform.origin)<5):
+		var direction = (player.global_transform.origin - global_transform.origin).normalized()
 		move_and_slide(direction * botSpeed)
 
 		for i in get_slide_count():
@@ -28,8 +17,8 @@ func _physics_process(delta):
 			# if collision.collider.name == "Player":
 			var object = collision.collider
 			if object.is_in_group("player"):
-#				object.die()
-				pass
+				object.die()
+			
 
 
 func _ready():
