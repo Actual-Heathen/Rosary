33377753aacfa50f49ad5c69910dd47a08e02bef
ZXenon <zinnwg@gmail.com>
2022-01-30 08:32:59 -0500

    created pause menu
    resume, restart, options, and return to main menu button created

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -8,13 +8,16 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	$VBoxContainer/StartButton.grab_focus()
 
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://scenes/garden.tscn")
+
+
+func _on_OptionsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
