df54152de61ef239fa4c1916592244a8c69478cd
Hayden Estes <hayden@Hayden-T440p>
2022-01-29 22:53:05 -0600

    added exit generation

--- scripts/map_builder.gd
+++ scripts/map_builder.gd
@@ -1,12 +1,14 @@
 extends Node
 
-
+export(String) var exit_scene
 export(int) var width  
 export(int) var height
 export(int) var density
 var maptrix = []
 var start_x
 var start_y
+var last_x
+var last_y
 export(int) var room_x
 export(int) var room_y 
 
@@ -27,6 +29,7 @@ func _ready():
  #now that the matrix is complete, time to build the rooms based on the matrix
 	place_rooms() #create dynamic_room children in the correct spots
 	spawn_player()
+	spawn_exit()
 
 
 #create the 2d matrix with 'width' and 'height'
@@ -118,11 +121,11 @@ func crop_matrix():
 					if safe == true:
 						maptrix[x][y] = 2
 						updated_last = true
+						last_x = x
+						last_y = y
 					elif safe == false && updated_last == false:
 						maptrix[x][y] = 0
 					
-					
-
 		
 
 
@@ -142,9 +145,16 @@ func spawn_player():
 	var player_prefab = preload("res://prefabs/player.tscn")
 	var player = player_prefab.instance()
 	add_child(player)
-	player.translation = Vector3(start_x*room_x,50,start_y*room_y)
+	player.translation = Vector3(start_x*room_x,1,start_y*room_y)
 	#player.translation = Vector3(0,0,0)
 
+# spawn the scene_loader at the ending room	
+func spawn_exit():
+	var exit_prefab = preload("res://prefabs/scene_loader.tscn")
+	var exit = exit_prefab.instance()
+	add_child(exit)
+	exit.path_to_scene  = (exit_scene)
+	exit.translation = Vector3(last_x*room_x,1,last_y*room_y)
 				
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
